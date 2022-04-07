70abb3a25c5e8dd98fbc99d319c29a80c10dd955
Ryan Crespo <crespoffi@gmail.com>
2022-04-06 20:28:35 -0400

    assignments

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -21,7 +21,41 @@
 
 // console.log(myFirstJob);
 
+// // Math Operators
+// const now = 2037;
+// const ageRyan = now - 1989;
+// const ageSarah = now - 2018;
+// console.log(ageRyan, ageSarah);
+
+// console.log(ageRyan * 2, ageRyan / 2, 2 ** 3);
+// // 2 ** 3 ==> 2 to the power of 3 ==> 2 * 2 * 2
+
+// const firstName = 'Ryan';
+// const lastName = 'Crespo';
+// console.log(firstName + " " + lastName);
+
+// // Assignment Operators
+// let x = 10 + 5; // ==> 15
+// x += 10; // ==> x = x + 10 = 25
+// x *= 4; // ==> x = x * 4 = 100
+// x++; // ==> x = x + 1
+// x--; // ==> x = x - 1
+// console.log(x);
+
+// // Comparison Operators
+// console.log(ageRyan > ageSarah); // >, <, >=, <=
+// console.log(ageSarah >= 18);
+
+// const isFullAge = ageSarah >= 18;
+
+// console.log(now - 1991 > now - 2018)
+
 const now = 2037;
 const ageRyan = now - 1989;
 const ageSarah = now - 2018;
-console.log(ageRyan, ageSarah);
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
