9c01fdb0b86fbbd42f3a73a1be0ed8458f260e16
Ryan Crespo <crespoffi@gmail.com>
2022-04-12 17:29:50 -0400

    finished part one

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -79,3 +79,37 @@ if (language === 'english' && population < 50 && !isIsland) {
     console.log(`${country} does not meet your criteria :(`)
 };
 */
+
+
+// // The Swicth Statement
+// const language = 'english';
+
+// switch(language) {
+//     case 'chinese':
+//     case 'mandarin':
+//         console.log('MOST number of native speakers!');
+//         break;
+//     case 'spanish':
+//         console.log('2nd place in number of native speakers');
+//         break;
+//     case 'english':
+//         console.log('3rd place');
+//         break;
+//     case 'hindi':
+//         console.log('Number 4');
+//         break;
+//     case 'arabic':
+//         console.log('5th most spoken language');
+//         break;
+//     default:
+//         console.log('Great language too :D');
+// };
+
+
+// The Conditional (Ternary) Operator
+// const population = 300;
+// const country = 'USA';
+
+// // population > 33 ? console.log('USA population is above average') : console.log('USA population is below average');
+// // const pop = population > 33 ? 'USA population is above average' : 'USA population is below average';
+// console.log(`${country}'s population is ${population > 33 ? 'above' : 'below'} average`);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -63,7 +63,6 @@
 // const averageAge = (ageRyan + ageSarah) / 2;
 // console.log(ageRyan, ageSarah, averageAge);
 
-
 ///////////////////////////////////
 // CODING CHALLENGE #1
 
@@ -119,7 +118,7 @@ GOOD LUCK 😀
 // const birthYear = 1989;
 // const year = 2022;
 
-// const ryan = "I'm " + firstName + ', a ' + (year - birthYear) + ' years old ' + job + '!'; // ==> old way...don't do this! 
+// const ryan = "I'm " + firstName + ', a ' + (year - birthYear) + ' years old ' + job + '!'; // ==> old way...don't do this!
 // console.log(ryan);
 
 // const ryanNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`
@@ -150,7 +149,6 @@ GOOD LUCK 😀
 // };
 // console.log(century);
 
-
 ////////////////////////////////////
 // Coding Challenge #2
 
@@ -169,86 +167,281 @@ GOOD LUCK 😀
 //     console.log(`Mark's BMI (${markBMI1}) is higher than John's BMI (${johnBMI1})!`);
 // } else {
 //     console.log(`John's BMI (${johnBMI1}) is higher than Mark's BMI (${markBMI1})!`);
-// }; 
+// };
 
+// END CODING CHALLEGE #2
 /////////////////////////////////////
-
 
 // Type Conversion & Type Coercion
 // --------------------------
 // type conversion ======
-const inputYear = '1991';
-console.log(Number(inputYear), inputYear); // ==> ****Number()**** converts string into number!!
-console.log(Number(inputYear) + 18);
+// const inputYear = "1991";
+// console.log(Number(inputYear), inputYear); // ==> ****Number()**** converts string into number!!
+// console.log(Number(inputYear) + 18);
 
-console.log(Number('Ryan'));
-console.log(typeof NaN);
+// console.log(Number("Ryan"));
+// console.log(typeof NaN);
 
-console.log(String(23), 23); // ==> ****String()**** converts number into a string!!
+// console.log(String(23), 23); // ==> ****String()**** converts number into a string!!
 
-// type coercion ======
-console.log('I am ' + 23 +' years old');
-console.log('23' - '10' - 3); // - changes it to number
-console.log('23' + '10' + 3); // + leaves it as a string
-console.log('23' * '2'); // * & / changes it to a number
+// // type coercion ======
+// console.log("I am " + 23 + " years old");
+// console.log("23" - "10" - 3); // - changes it to number
+// console.log("23" + "10" + 3); // + leaves it as a string
+// console.log("23" * "2"); // * & / changes it to a number
 
+// let n = "1" + 1; // '11'
+// n = n - 1; // 11 - 1
+// console.log(n); // 10
 
-let n = '1' + 1; // '11'
-n = n - 1; // 11 - 1
-console.log(n); // 10
-
-console.log(2 + 3 + 4 + '5'); // when using '+' it will any numbers that come before the string then add the string to the end ====>> 2 + 3 = 5 + 4 = 9 + '5' = 95
-console.log('10' - '4' - '3' - 2 + '5'); // '10' - '4' is the same as 10 - 4...so on and so on the add string to the end ====>> ....15
-
+// console.log(2 + 3 + 4 + "5"); // when using '+' it will any numbers that come before the string then add the string to the end ====>> 2 + 3 = 5 + 4 = 9 + '5' = 95
+// console.log("10" - "4" - "3" - 2 + "5"); // '10' - '4' is the same as 10 - 4...so on and so on the add string to the end ====>> ....15
 
 // Truthy and Falsy Value
 // -------------------------
 // ===== 5 falsy values: 0, '', undefined, null, NaN
-console.log(Boolean(0)); // false
-console.log(Boolean(undefined)); // false
-console.log(Boolean('Ryan')); // true
-console.log(Boolean({})); // true
-console.log(Boolean('')); // false
+// console.log(Boolean(0)); // false
+// console.log(Boolean(undefined)); // false
+// console.log(Boolean("Ryan")); // true
+// console.log(Boolean({})); // true
+// console.log(Boolean("")); // false
 
-const money = 0; // 0 is a falsy value
-if (money) {
-    console.log("Don't spend it all!");
-} else {
-    console.log("you should get a job!");
-};
-// console.log(typeof money);
+// const money = 0; // 0 is a falsy value
+// if (money) {
+//   console.log("Don't spend it all!");
+// } else {
+//   console.log("you should get a job!");
+// }
+// // console.log(typeof money);
 
-let height; // no value so this is undefined and undefined is a falsy value!
-if (height) {
-    console.log('Height is defined!!');
-} else {
-    console.log('Height is UNDEFINED!');
-};
-
+// let height; // no value so this is undefined and undefined is a falsy value!
+// if (height) {
+//   console.log("Height is defined!!");
+// } else {
+//   console.log("Height is UNDEFINED!");
+// }
 
 // Equality Operators: == and ===
 // ----------------------------
-const age = 18;
-if (age === 18) console.log('You are now an adult! (strict)'); // If () statements do not need {} if it is only one condition
-// === is the strict equality operator. Only returns true when both values are exactly the same. Example below 👇
-console.log(18 === 18); // true -----> both have the same value and the same type
-console.log(19 === 18); // false ------> both are the same type but don't have the same value
-console.log('10' === '10'); // true ------> string is equal to a string
-console.log('10' === 10); // false -----> string is not equal to a number
-// == does type coercion. Examples:
-console.log('18' == 18); // true ------> == converts '18' into a number
-if (age == 18) console.log('You are now an adult! (loose)');
+// const age = 18;
+// if (age === 18) console.log("You are now an adult! (strict)"); // If () statements do not need {} if it is only one condition
+// // === is the strict equality operator. Only returns true when both values are exactly the same. Example below 👇
+// console.log(18 === 18); // true -----> both have the same value and the same type
+// console.log(19 === 18); // false ------> both are the same type but don't have the same value
+// console.log("10" === "10"); // true ------> string is equal to a string
+// console.log("10" === 10); // false -----> string is not equal to a number
+// // == does type coercion. Examples:
+// console.log("18" == 18); // true ------> == converts '18' into a number
+// if (age == 18) console.log("You are now an adult! (loose)");
 
-const favorite = Number(prompt("What's your favorite number?"));
-console.log(favorite);
-console.log(typeof favorite);
+// const favorite = Number(prompt("What's your favorite number?"));
+// console.log(favorite);
+// console.log(typeof favorite);
 
-if (favorite === 23) { // 23 === 23 ---> TRUE
-    console.log('Cool! 23 is an amazing number!');
-} else if (favorite === 7) {
-    console.log('7 is also a cool number');
-} else {
-    console.log('Number is not 23 or 7');
-};
+// if (favorite === 23) {
+//   // 23 === 23 ---> TRUE
+//   console.log("Cool! 23 is an amazing number!");
+// } else if (favorite === 7) {
+//   console.log("7 is also a cool number");
+// } else {
+//   console.log("Number is not 23 or 7");
+// }
 
-if (favorite !== 23) console.log('Why not 23?');
+// if (favorite !== 23) console.log("Why not 23?");
+
+// Logical Operators
+// ----------------------
+// const hasDL = true; // A
+// const hasGoodVision = true; // B
+// // const hasGoodVision = false; // B
+// const isTired = false; // C
+
+// console.log(hasDL && hasGoodVision);
+// console.log(hasDL || hasGoodVision);
+// console.log(!hasDL);
+
+// const shouldDrive = hasDL && hasGoodVision;
+
+// // if (shouldDrive) {
+// //   console.log('Sarah is able to drive');
+// // } else {
+// //   console.log('Someone else should drive...');
+// // };
+
+// console.log(hasDL && hasGoodVision && isTired); // when any one of the variables is false, this whole statement becomes false. All must be true for statement to be true
+
+// if (hasDL && hasGoodVision && !isTired) {
+//   console.log('Sarah is allowed to drive!');
+// } else {
+//   console.log('Someone else should drive...');
+// };
+
+////////////////////////////////////
+// Coding Challenge #3
+
+/*
+There are two gymnastics teams, Dolphins and Koalas. They compete against each other 3 times. The winner with the highest average score wins the a trophy!
+
+1. Calculate the average score for each team, using the test data below
+2. Compare the team's average scores to determine the winner of the competition, and print it to the console. Don't forget that there can be a draw, so test for that as well (draw means they have the same average score).
+
+3. BONUS 1: Include a requirement for a minimum score of 100. With this rule, a team only wins if it has a higher score than the other team, and the same time a score of at least 100 points. HINT: Use a logical operator to test for minimum score, as well as multiple else-if blocks 😉
+4. BONUS 2: Minimum score also applies to a draw! So a draw only happens when both teams have the same score and both have a score greater or equal 100 points. Otherwise, no team wins the trophy.
+
+TEST DATA: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+TEST DATA BONUS 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+TEST DATA BONUS 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+
+GOOD LUCK 😀
+*/
+
+// // Test Case 1:
+// const dolphinAvg1 = (96 + 108 + 89) / 3;
+// const koalaAvg1 = (88 + 91 + 110) / 3;
+
+// console.log(dolphinAvg1, koalaAvg1);
+
+// if (dolphinAvg1 > koalaAvg1) {
+//   console.log("Dolphin's are the winners!");
+// } else if (dolphinAvg1 === koalaAvg1) {
+//   console.log('It is a draw!');
+// } else if (koalaAvg1 > dolphinAvg1) {
+//   console.log("Koala's are the winners!");
+// };
+
+// // Bonus 1:
+// const dolphinAvg2 = (97 + 112 + 101) / 3;
+// const koalaAvg2 = (109 + 95 + 123) / 3;
+
+// console.log(dolphinAvg2, koalaAvg2);
+
+// if (dolphinAvg2 > koalaAvg2 && dolphinAvg2 >= 100) {
+//   console.log("Dolphin's are the winners!!");
+// } else if (dolphinAvg2 === koalaAvg2 && dolphinAvg2 >= 100 && koalaAvg2 >= 100) {
+//   console.log('It is a draw!!');
+// } else if (koalaAvg2 > dolphinAvg2 && koalaAvg2 >= 100) {
+//   console.log("Koala's are the winners!!");
+// } else {
+//   console.log("Doesn't meet the qualifications!");
+// };
+
+// // Bonus 2:
+// const dolphinAvg3 = (97 + 112 + 101) / 3;
+// const koalaAvg3 = (109 + 95 + 106) / 3;
+
+// console.log(dolphinAvg3, koalaAvg3);
+
+// if (dolphinAvg3 > koalaAvg3 && dolphinAvg3 >= 100) {
+//   console.log("Dolphin's are the winners!!!");
+// } else if (dolphinAvg3 === koalaAvg3 && dolphinAvg3 >= 100 && koalaAvg3 >= 100) {
+//   console.log('It is a draw!!!');
+// } else if (koalaAvg3 > dolphinAvg3 && koalaAvg3 >= 100) {
+//   console.log("Koala's are the winners!!!");
+// } else {
+//   console.log("Doesn't meet the qualifications!");
+// };
+
+// END CODING CHALLENGE #3
+////////////////////////////////////////
+
+// The Switch Statement
+// ----------------------------
+// const day = 'saturday';
+
+// switch(day) {
+//   case 'monday': // day === 'monday'
+//     console.log('Plan course structure');
+//     console.log('Go to coding meetup');
+//     break;
+//   case 'tuesday':
+//     console.log('Prepare theory videos');
+//     break;
+//   case 'wednesday':
+//   case 'thursday':
+//     console.log('Write code examples');
+//     break;
+//   case 'friday':
+//     console.log('Record videos');
+//     break;
+//   case 'saturday':
+//   case 'sunday':
+//     console.log('Enjoy the weekend');
+//     break;
+//   default:
+//     console.log('Not a valid day');
+// };
+
+// if (day === 'monday') {
+//   console.log('Plan course structure');
+//   console.log('Go to coding meetup');
+// } else if (day === 'tuesday') {
+//     console.log('Prepare theory videos');
+// } else if (day === 'wednesday' || day === 'thursday') {
+//     console.log('Write code examples');
+// } else if (day === 'friday') {
+//     console.log('Record videos');
+// } else if (day === 'saturday' || day === 'sunday') {
+//   console.log('Enjoy the weekend');
+// } else {
+//   console.log('Not a valid day');
+// }
+
+// Statements and Expressions
+// -------------------------------
+// 3 + 4 // This is an expression as it provides a value
+// 1991 // Also an expression. Produces a value as well
+// true && false && !false // Produces a Boolean value therefore is also an expression
+
+// if (23 > 10) {
+//   const str = '23 is bigger';
+// }; // ^--This entire "if() statement" is an example of a statement. Doesn't produce a value.
+
+// console.log(`I'm ${2037 - 1991} years old.`); // Template literals only accept expressions.
+
+// The Conditional (Ternary) Operator
+// -------------------------------------
+// const age = 19;
+// age >= 18 ? console.log('I like to drink wine!') : console.log('I like to drink water!'); // condition ? if statement (truthy) : else (falsy)
+
+// let drink2;
+// if (age >= 18) {
+//   drink2 = 'wine';
+// } else {
+//   drink2 = 'water';
+// };
+// console.log(drink2);
+
+// const drink = age >= 18 ? 'wine' : 'water'; // Ternary operator. Much cleaner way of doing the aove if/else statement, and its shorter
+// console.log(drink); // Ternary operators are expressions!! can be used in template literals unlike if/else statements
+
+// console.log(`I like to drink ${age >= 18 ? 'wine' : 'water'}`);
+
+//////////////////////////////////////////
+// CODING CHALLENGE #4
+
+/*
+Steven wants to build a very simple tip calculator for whenever he goes eating in a resturant. In his country, it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%.
+
+1. Your task is to caluclate the tip, depending on the bill value. Create a variable called 'tip' for this. It's not allowed to use an if/else statement 😅 (If it's easier for you, you can start with an if/else statement, and then try to convert it to a ternary operator!)
+2. Print a string to the console containing the bill value, the tip, and the final value (bill + tip). Example: 'The bill was 275, the tip was 41.25, and the total value 316.25'
+
+TEST DATA: Test for bill values 275, 40 and 430
+
+HINT: To calculate 20% of a value, simply multiply it by 20/100 = 0.2
+HINT: Value X is between 50 and 300, if it's >= 50 && <= 300 😉
+
+GOOD LUCK 😀
+*/
+const bill = 275;
+const tip = bill >= 50 && bill <= 300 ? (15 / 100) * bill : (20 / 100) * bill;
+console.log(`bill: ${bill}, tip: ${tip}, total: ${bill + tip}`);
+
+const bill2 = 40;
+const tip2 =
+  bill2 >= 50 && bill2 <= 300 ? (15 / 100) * bill2 : (20 / 100) * bill2;
+console.log(`bill: ${bill2}, tip: ${tip2}, total: ${bill2 + tip2}`);
+
+const bill3 = 430;
+const tip3 =
+  bill3 >= 50 && bill3 <= 300 ? (15 / 100) * bill3 : (20 / 100) * bill3;
+console.log(`bill: ${bill3}, tip: ${tip3}, total: ${bill3 + tip3}`);
