ad026826cc3ea6ad235ba248edf9aba86ae6def6
Ryan Crespo <crespoffi@gmail.com>
2022-05-16 17:59:27 -0400

    coding challenge

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -83,7 +83,7 @@ const measureKelvin = function () {
     // 3) FIX THE BUG ==> NOW THAT YOU FOUND THE BUG YOU CAN GO BACK IN YOUR CODE AND FIX IT
     // value: Number(prompt('Degrees in Celsius:')), // the prompt function always returns a string by default which is why we get back 10273. ("10" + "273") = "10273"
     // added Number() in the prompt to convert the string to a number
-    value: 10
+    value: 10,
   };
 
   // 2) FIND THE BUG: ==> CONSOLE.LOG() TO FIND THE BUG IN YOUR CODE
@@ -99,31 +99,74 @@ const measureKelvin = function () {
 // 1) IDENTIFY THE BUG: ==> SEE A BUG IN THE FINAL CODE
 console.log(measureKelvin());
 
-// DEBUGGING IN CHROME: 
+// DEBUGGING IN CHROME:
 // go into your dev tools and open the "sources" tab then click on the script where the bug is. (scrpt.js for this example)
 // now when we our in our code in the dev tools we can set "break points." To toggle the break point simply click on the empty space to the left of the code line.
-// Break points cause the code execution to stop at that particular point. 
+// Break points cause the code execution to stop at that particular point.
 
 // ======================================================
 // USING A DEBUGGER:
 const calcTempAmplitudeBug = function (t1, t2) {
-    const temps = t1.concat(t2); // concat is taking array 1 [t1] and array two [t2] and merging them together to create a whole new array [temps]
-    console.log(temps);
-  
-    let max = 0;
-    let min = 0;
-  
-    for (let i = 0; i < temps.length; i++) {
-      const curTemp = temps[i];
-  
-      if (typeof curTemp !== 'number') continue;
-  
-      if (curTemp > max) max = curTemp;
-      if (curTemp < min) min = curTemp;
-    }
-    console.log('max:', max);
-    console.log('min:', min);
-    return max - min;
-  };
-  const amplitudeBug = calcTempAmplitudeBug([3, 5, 7], [9, 4, 5]);
-  console.log(amplitudeBug);
+  const temps = t1.concat(t2); // concat is taking array 1 [t1] and array two [t2] and merging them together to create a whole new array [temps]
+  console.log(temps);
+
+  let max = 0;
+  let min = 0;
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+
+    if (typeof curTemp !== 'number') continue;
+    // debugger; // this is a statement built into javascript that tells the browser to open the debugger tool on chrome
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log('max:', max);
+  console.log('min:', min);
+  return max - min;
+};
+const amplitudeBug = calcTempAmplitudeBug([3, 5, 7], [9, 4, 5]);
+console.log(amplitudeBug);
+
+// =================================================================
+// Coding Challenge:
+
+/*
+Given an array of forecasted maximum temperatures, the thermometer displays a string with these temperatures.
+
+Example: [17, 21, 23] will print "... 17ºC in 1 days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+
+Create a function 'printForecast' which takes in an array 'arr' and logs a string like the above to the console.
+
+Use the problem-solving framework: Understand the problem and break it up into sub-problems!
+
+TEST DATA 1: [17, 21, 23]
+TEST DATA 2: [12, 5, -5, 0, 4]
+*/
+
+// 1) Unserstanding the problem:
+// -- Array to string seperated by "..."
+// -- What is the x days? Answer: the days are the index of the arrays + 1
+
+// 2) Breaking up the problem into sub-problems:
+// -- How to turn array into string
+// -- Transform each element in array into string with ºC
+// -- The string needs to have the day as well (index + 1)
+// -- Add "..." between elements and start at end of string
+// -- Log string to the console
+
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+
+// console.log(`... ${data1[0]}ºC, ... ${data1[1]}ºC, ... ${data1[2]}ºC`); // just to see if the indexing works and we get back the right numbers
+
+function printForecast(arr) {
+  let str = '';
+  for (let i = 0; i < arr.length; i++) {
+    // basic loop into our two arrays
+    str += `${arr[i]}ºC in ${i + 1} days ... `; // here we are taking "str" and +=(increment by one) through the array ==> (data1...17, 21, 23) ==> and adding the index + 1 to the string to get the days back
+  }
+  console.log('... ' + str); // we add '... ' to str because that is what the challenge is asking for. If we just do str without the '...' in the beginning we won't get the ... to start off with
+}
+printForecast(data1); // here we are calling the function and inputting our arrays [data1 & data2] into the function
+printForecast(data2);
