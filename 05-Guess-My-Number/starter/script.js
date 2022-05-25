771371e538378c8604e7d0b48017c9bd7a591ef4
Ryan Crespo <crespoffi@gmail.com>
2022-05-24 20:48:13 -0400

    creating mini game

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,4 @@
 'use strict';
+
+document.querySelector('.message'); // '.message' is a part of DOM manipulation and we use the same property from css to access classes and ids.
+console.log(document.querySelector('.message').textContent); // textContent will display only the text within that id or class.
