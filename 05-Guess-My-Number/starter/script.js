dc04aa71549e899794ddb3fe28436121fcc60c85
Ryan Crespo <crespoffi@gmail.com>
2022-05-25 01:01:53 -0400

    practicing dom manipulation

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,4 +1,12 @@
 'use strict';
 
-document.querySelector('.message'); // '.message' is a part of DOM manipulation and we use the same property from css to access classes and ids.
+// document.querySelector('.message'); // '.message' is a part of DOM manipulation and we use the same property from css to access classes and ids.
 console.log(document.querySelector('.message').textContent); // textContent will display only the text within that id or class.
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+console.log(document.querySelector('.message').textContent); // and now it shows the new content which we added in the line above instead of the original html content
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 25;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
