64069b82198b41f0fdf3ed7481d9212e80554e45
Ryan Crespo <crespoffi@gmail.com>
2022-06-04 22:44:04 -0400

    daily javascript practice

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,10 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsShowModal = document.querySelectorAll('.show-modal');
+console.log(btnsShowModal);
+
+for (let i = 0; i < btnsShowModal.length; i++)
+  console.log(btnsShowModal[i].textContent); // display the current 'show-modal' text content (show modal 1, show modal 2...)
+// because there is only one line of code if the loop, we don't need the curly braces {} after the for loop
