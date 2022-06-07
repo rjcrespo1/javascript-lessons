db08a1e2e7318107e5674b376d48359b63c1c974
Ryan Crespo <crespoffi@gmail.com>
2022-06-06 21:05:55 -0400

    daily javascript practice

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0'); // get the id by using "#"
 const score1El = document.getElementById('score--1'); // another way to get the id
 const current0El = document.getElementById('current--0');
@@ -15,7 +17,9 @@ score0El.textContent = 0; // We're just changing the displayed numbers on the pa
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0; // this variable has to be set outside the function below because if it was in the function, the score would be reset to 0 every time the button is clicked
+let activePlayer = 0; // variable for selecting the current player
 
 // Rolling the dice functionality
 btnRoll.addEventListener('click', function () {
@@ -32,8 +36,14 @@ btnRoll.addEventListener('click', function () {
     // Add the dice to the current score
     // *** currentScore = currentScore + dice;
     currentScore += dice; // this is a cleaner way of writing the code above
-    current0El.textContent = currentScore; // CHANGE LATER
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore; // Here we are updating the current score to the current player{activePlayer}
   } else {
     // Switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0; // if the active player is 0, it then turns to player 1 and vice versa
+    player0El.classList.toggle('player--active'); // "toggle" adds the class if it is not there and if it is, it removes the class
+    player1El.classList.toggle('player--active');
   }
 });
